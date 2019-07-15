3f7781918931465387ba2bb5f2a75e98cde09b2f
saeel <16co35@aitdgoa.edu.in>
2019-07-15 16:01:03 +0530

    updation is done

--- exp1/1.c
+++ exp1/1.c
@@ -2,7 +2,7 @@
 
 int main(){
 	int i;
-	int j=0;
+//	int j=0;
 	char arr[10];
     
 	printf("Enter the String value\n");
@@ -12,12 +12,12 @@ int main(){
 }
 
 for(i=0;i<10;i++ ){
-	if(arr[i]==97+j){
+	if(arr[i]==97 ||arr[i]==98 ||arr[i]==99 ||arr[i]==100 ||arr[i]==101 ||arr[i]==102 ||arr[i]==103 ||arr[i]==104 ||arr[i]==105 ||arr[i]==106 ||arr[i]==107 ||arr[i]==108 ||arr[i]==109 ||arr[i]==110 ||arr[i]==111 ||arr[i]==112 ||arr[i]==113 ||arr[i]==114 ||arr[i]==115 ||arr[i]==116 ||arr[i]==117 ||arr[i]==118 ||arr[i]==119 ||arr[i]==120 ||arr[i]==121 ||arr[i]==122){
 	printf("%c",arr[i]);
 	printf("->char\n");
 	
 }
-j++;
+
 }
 
 return 0;
